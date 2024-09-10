12eaeb0f994e1df5757837ad24839c69c0858bd1
HSSBoston <74394277+HSSBoston@users.noreply.github.com>
2024-09-10 09:56:48 -0400

    constellation size extraction

--- opencv/extract_stars.py
+++ opencv/extract_stars.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from detect_contours import detectContours
+from star import Star
 
 def extractStars(origFileNameWithoutExt):
     origFileName = origFileNameWithoutExt + ".jpg"
@@ -20,26 +21,24 @@ def extractStars(origFileNameWithoutExt):
             (x,y), radius = cv2.minEnclosingCircle(approxPoly)
             center = (int(x), int(y))
             radius = int(radius)
-            shapes.append([center, radius])
             if radius >= 8:
                 shapeName = "big-circle"
-                print("big circle", center, radius)
             else:
                 shapeName = "small-circle"
-                print("small circle", center, radius)
-            star = Star(shapeName, center
+            star = Star(shapeName, center,
                         center[0]-radius, center[0]+radius,
-                        center[1]-radius, center[0]+radius)
+                        center[1]-radius, center[1]+radius)
         else:
             x, y, width, height = cv2.boundingRect(approxPoly)
             center = (int(x+(width/2)), int(y+(height/2)))
             halfSideLength = int((width + height)/4)
-            star.
-            stars.append([center, halfSideLength])
-            
-            print("rectangle", center, halfSideLength)
+            star = Star("rectangle", center,
+                        center[0]-halfSideLength, center[0]+halfSideLength,
+                        center[1]-halfSideLength, center[1]+halfSideLength)
 
-    return shapes, grayscaleImage, binaryImage
+        stars.append(star)
+
+    return stars, grayscaleImage, binaryImage
 
 if __name__ == "__main__":
 #     origFileNameWithoutExt = "images/aquila"
@@ -51,6 +50,9 @@ if __name__ == "__main__":
     origFileNameWithoutExt = "images/orion"
     
     print(origFileNameWithoutExt)
-    extractStars(origFileNameWithoutExt)
+    stars, _, _ = extractStars(origFileNameWithoutExt)
     
+    for star in stars:
+        print( star.shape, star.center,
+               star.xLeft, star.xRight, star.yTop, star.yBottom)
 
--- opencv/star.py
+++ opencv/star.py
@@ -2,29 +2,11 @@
 
 
 class Star:
-    def __init__(shape, center, xLeft, xRight, yTop, yBottom):
+    def __init__(self, shape, center, xLeft, xRight, yTop, yBottom):
         self.shape = shape # "big-circle" "small-circle" or "rectangle"
         self.center = center
         self.xLeft = xLeft
         self.xRight = xRight
         self.yTop = yTop
         self.yBottom = yBottom
-    
-    def shape():
-        return self.shape
-    
-    def center():
-        return self.center
-    
-    def xLeft():
-        return self.xLeft
-    
-    def xRight():
-        return self.xRight
-    
-    def yTop():
-        return self.yTop
-    
-    def yBottom():
-        return self.yBottom
     
