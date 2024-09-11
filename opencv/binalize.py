e2e18013d0ff3e425937c7855cae113c7eac782e
HSSBoston <74394277+HSSBoston@users.noreply.github.com>
2024-09-11 14:38:41 -0400

    binalization updated

--- opencv/binalize.py
+++ opencv/binalize.py
@@ -10,13 +10,14 @@ def binalizeImage(origFileNameWithoutExt):
     grayscaleImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         # Convert each pixel's color to grayscale.
     cv2.imwrite(origFileNameWithoutExt + "-gray.jpg", grayscaleImage)
+        # Save the grayscaled image as *-gray.jpg.
 
     _, binaryImage = cv2.threshold(grayscaleImage, 127, 255, cv2.THRESH_BINARY)
-        # binarization threshold: 127
-        # If a pixel value is > threshold (127), the value becomes the max val (255).
+        # Binarization threshold: 127
+        # If a pixel's value > threshold (127), the value is replaced with the max val (255).
         # Otherwise, it becomes 0. 
     cv2.imwrite(origFileNameWithoutExt + "-binary.jpg", binaryImage)
-    
+        # Save the binalized image as *-binary.jpg.
     return (grayscaleImage, binaryImage)
 
 if __name__ == "__main__":
@@ -26,6 +27,17 @@ if __name__ == "__main__":
 #     origFileNameWithoutExt = "images/cassiopeia"
 #     origFileNameWithoutExt = "images/cygnus"
 #     origFileNameWithoutExt = "images/lyra"
-    origFileNameWithoutExt = "images/orion"
-    
-    binalizeImage(origFileNameWithoutExt)
+#     origFileNameWithoutExt = "images/orion"
+# 
+#     binalizeImage(origFileNameWithoutExt)
+
+    origFileNamesWithoutExt = ["images/aquila",
+                               "images/big-dipper",
+                               "images/canis-major",
+                               "images/cassiopeia",
+                               "images/cygnus",
+                               "images/lyra",
+                               "images/orion",
+                               "images/canis-minor"]
+    for fileName in origFileNamesWithoutExt:
+        binalizeImage(fileName)
