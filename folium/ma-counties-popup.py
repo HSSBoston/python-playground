0c704243ea630b9baaa87a361c2595d2a6b027b9
HSSBoston <74394277+HSSBoston@users.noreply.github.com>
2024-09-20 09:59:49 -0400

    popups

--- folium/ma-counties-popup.py
+++ folium/ma-counties-popup.py
@@ -1,4 +1,5 @@
-import folium, json
+import folium
+import folium.features
 
 geoJsonFileName = "COUNTIES_POLYM.json"
 
@@ -6,16 +7,24 @@ logan = (42.365792221496, -71.00966724673485)
 
 bostonMap = folium.Map(location=logan, zoom_start=11)
 
-folium.GeoJson(
+
+gJson= folium.GeoJson(
     geoJsonFileName,
     style_function=lambda feature: {
         "weight": 2,
         "fillColor": "green"
             if feature["properties"]["COUNTY"].lower() == "middlesex"
-            else "blue",
-        "fillOpacity": 0.1
+            else "transparent",
+        "fillOpacity": 0.1,
     },
+#     popup=popup,
 ).add_to(bostonMap)
+
+popup = folium.features.GeoJsonPopup(
+    fields=["COUNTY"],
+    labels=False
+).add_to(gJson)
+
 
 # folium.GeoJson(
 #     geoJsonFileName,
