a241906f89336fb675e1bf4b4b1ec070c7557861
HSSBoston <74394277+HSSBoston@users.noreply.github.com>
2024-09-22 07:33:52 -0400

    tkinter init

--- tkinter/tkinter-first.py
+++ tkinter/tkinter-first.py
@@ -1,7 +1,23 @@
-from tkinter import *
+import tkinterweb
+import tkinter
 
-root = Tk()
-root.title("はじめてのtkinter")
 
-##ウィンドウの表示
+def load_website():
+    frame.load_website(entry.get())
+
+
+root = tkinter.Tk()
+root.title('MiniBrowser')
+
+tkinter.Label(master=root, text='> Browse some website :D <').pack()
+
+entry = tkinter.Entry(master=root)
+entry.pack()
+
+button = tkinter.Button(master=root, text='Go!', command=load_website)
+button.pack()
+
+frame = tkinterweb.HtmlFrame(master=root)
+frame.pack()
+
 root.mainloop()
