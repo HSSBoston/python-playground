fea50af0c9d34bcc7ee1e7bf4252fdfa2860e924
HSSBoston <74394277+HSSBoston@users.noreply.github.com>
2024-09-23 06:03:28 -0400

    wa map added

---
+++ folium/wa-cities.py
@@ -0,0 +1,47 @@
+# Font choices: https://fontawesome.com/
+#
+# Boundarires of WA counties (shepefiles/GeoJson):
+#   https://geo.wa.gov/datasets/wadnr::wa-county-boundaries/about
+#
+# Shapefiles -> GeoJson (and GeoJson -> GeoJson) conversion: 
+#   https://mapshaper.org/
+#     mapshaper -proj wgs84 -simplify dp 20% -o COUNTIES_POLYM.json
+#   GDAL's ogr2ogr command:
+#     ogr2ogr -f GeoJSON -t_srs crs:84 COUNTIES_POLYM.geojson COUNTIES_POLYM.shp
+
+import folium
+
+geoJsonFileName = "counties_wa.json"
+waCenter = (47.7511, -120.7401)
+
+waMap = folium.Map(location = waCenter, zoom_start = 7, tiles="OpenTopoMap")
+
+waCountiesLayer = folium.GeoJson(
+    geoJsonFileName,
+    style_function=lambda feature: {
+        "color": "darkblue",
+        "weight": 2,
+        "fillColor": "transparent",
+#         "fillColor": "lightblue"
+#             if feature["properties"]["COUNTY"] == "Middlesex"
+#             else "transparent",
+#         "fillOpacity": 0.1,
+    },
+).add_to(waMap)
+# 
+# for countyName, countyCenter in maCountyCenters.items():
+#     folium.Marker(
+#         countyCenter,
+#         popup = folium.Popup(
+#             f"<b>{countyName}</b><p>some extra info</p>",
+#             show = True,
+#             sticky = True,
+#             ),
+#     ).add_to(maMap)
+
+waMap.save("wa-counties.html")
+
+
+
+
+
