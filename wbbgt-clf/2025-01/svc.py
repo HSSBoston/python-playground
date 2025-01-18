56855e85fba5f4ff696cc1d0e5a38f8ab42fec0b
HSSBoston <hanna.s.suzuki@gmail.com>
2025-01-18 16:53:34 -0500

    svc added

--- wbbgt-clf/2025-01/svc.py
+++ wbbgt-clf/2025-01/svc.py
@@ -1,12 +1,10 @@
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split, StratifiedShuffleSplit, StratifiedKFold, cross_val_score
 from sklearn.metrics import f1_score, confusion_matrix
 from sklearn.inspection import permutation_importance
 from sklearn.tree import plot_tree
 from wbgt_metrics import f1_score_loose, f1_loose_scorer
 import numpy as np
-import matplotlib.pyplot as plt
-# import dtreeviz
 from dataset import readData
 
 datasetFileName = "dataset-downsampled.csv"
@@ -17,20 +15,15 @@ print(f"Number of feature sets: {len(X)}")
 
 X_train, X_test, y_train, y_test = train_test_split(X, y,
                                                     test_size=0.2, random_state=0)
-    # 30% for testing, 70% for training
-    # Deterministic (non-random) sampling
-dTree = DecisionTreeClassifier(random_state=0)
-    # Too shallow tree: poorer classification
-    # Too deep: overfitting
-dTree.fit(X_train, y_train)
-# dTree.fit(X, y)
+svc = SVC(random_state=0)
+svc.fit(X_train, y_train)
 
-accuracy = dTree.score(X_train, y_train)
+accuracy = svc.score(X_train, y_train)
 print (f"Accuracy in training: {round(accuracy,3)}")
-accuracy = dTree.score(X_test, y_test)
+accuracy = svc.score(X_test, y_test)
 print (f"Accuracy in testing: {round(accuracy,3)}")
 
-y_predicted = dTree.predict(X_test)
+y_predicted = svc.predict(X_test)
 f1score = f1_score(y_test, y_predicted, average="macro")
 print(f"F1 score: {round(f1score, 3)}")
 
@@ -43,36 +36,22 @@ print(f"F1 loose score: {round(f1LooseScore, 3)}")
 
 # K-fold validation
 skf = StratifiedKFold(n_splits=5)
-scores = cross_val_score(dTree, X, y, cv=skf, scoring="f1_macro")
+scores = cross_val_score(svc, X, y, cv=skf, scoring="f1_macro")
 print(f"Cross validation F1 score w/ StratifiedKFold: {round(np.mean(scores),3)}")
 
 sskf = StratifiedShuffleSplit(n_splits=10, test_size=0.2)
-scores = cross_val_score(dTree, X, y, cv=sskf, scoring="f1_macro")
+scores = cross_val_score(svc, X, y, cv=sskf, scoring="f1_macro")
 print(f"Cross validation F1 score w/ StratifiedShuffleSplit: {round(np.mean(scores),3)}")
 
-scores = cross_val_score(dTree, X, y, cv=skf, scoring=f1_loose_scorer)
+scores = cross_val_score(svc, X, y, cv=skf, scoring=f1_loose_scorer)
 print(f"Cross validation F1 loose score w/ StratifiedKFold: {round(np.mean(scores),3)}")
 
-scores = cross_val_score(dTree, X, y, cv=sskf, scoring=f1_loose_scorer)
+scores = cross_val_score(svc, X, y, cv=sskf, scoring=f1_loose_scorer)
 print(f"Cross validation F1 loose score w/ StratifiedShuffleSplit: {round(np.mean(scores),3)}")
 
+# print(svc.feature_importances_)
+# pImportance = permutation_importance(svc, X, y, n_repeats=100, random_state=0)
+# print(pImportance["importances_mean"])
 
-print(dTree.feature_importances_)
-pImportance = permutation_importance(dTree, X, y, n_repeats=100, random_state=0)
-print(pImportance["importances_mean"])
 
 
-# plot_tree(clf,
-#           feature_names=iris.feature_names,
-#           class_names=iris.target_names,
-#           fontsize=10,
-#           filled=True)
-# plt.show()
-# 
-# # viz_model = dtreeviz.model(clf,
-# #                X_train=X_train,
-# #                y_train=y_train,
-# #                target_name='Class',
-# #                feature_names=iris.feature_names,
-# #                class_names=iris.target_names)
-# # viz_model.view(scale=0.8)
