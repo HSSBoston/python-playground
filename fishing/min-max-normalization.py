c1426f76aaf46a0c51962e1c82aafafc085a2e6f
hssboston <hanna.s.suzuki@gmail.com>
2025-10-04 00:32:58 -0400

    min-max rader

--- fishing/min-max-normalization.py
+++ fishing/min-max-normalization.py
@@ -2,36 +2,29 @@ import csv
 
 inputFile = "Tasmania_5years.csv"
 
-def minMaxNormalization(row):
-    ts, ws, precip = float(row[2]), float(row[5]), float(row[9])
-#     print(yr, doy, ts, ws, precip)
-    tsNormalized = 1 - abs(21-ts)/11
-    if ws >= 6:
-        ws = 6
-    wsNormalized = 1 - abs(3-ws)/6
-    if precip >= 10:
-        precip = 10
-    precipNormalized = 1 - precip/10
-    return (tsNormalized, wsNormalized, precipNormalized)
-
-
 with open(inputFile, "r") as f:
     reader = csv.reader(f)
     header = next(reader)
 #     rows = [row for row in reader]
-    tsTotal = wsTotal = precipTotal = rowCount = 0
+    tempMaxTotal = tempMinTotal = precipTotal = rowCount = 0
     for row in reader:
-        ts, ws, precip = minMaxNormalization(row)
-        tsTotal += ts
-        wsTotal += ws
+        tempMax, tempMin, precip = float(row[7]), float(row[8]), float(row[9])
+        tempMaxTotal += tempMax
+        tempMinTotal += tempMin
         precipTotal += precip
         rowCount += 1
-tsAvg = tsTotal/rowCount
-wsAvg = wsTotal/rowCount
+tempMaxAvg = tempMaxTotal/rowCount
+tempMinAvg = tempMinTotal/rowCount
+tempDiffAvg = tempMaxAvg - tempMinAvg
 precipAvg = precipTotal/rowCount
-print(tsAvg, wsAvg, precipAvg)
+print("Daily avg:", "Max temp", tempMaxAvg, "Temp diff", tempDiffAvg, "Precip", precipAvg)
     
-        
+tempMaxNormalized = 1 - abs(21 - tempMaxAvg)/13
+
+if tempDiff > 11:
+    tempDiff = 11
+tempDiffNormalized = 1- tempDiff/11
+
 
 
 
--- fishing/rader.py
+++ fishing/rader.py
@@ -1,8 +1,33 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import csv, numpy as np, matplotlib.pyplot as plt
+
+inputFile = "Tasmania_5years.csv"
+
+def minMaxNormalization(row):
+    ts, ws, precip = float(row[2]), float(row[5]), float(row[9])
+    tsNormalized = 1 - abs(21-ts)/11
+    wsNormalized = 1 - abs(3-ws)/6
+    if precip >= 10:
+        precip = 10
+    precipNormalized = 1 - precip/10
+    return (tsNormalized, wsNormalized, precipNormalized)
+
+with open(inputFile, "r") as f:
+    reader = csv.reader(f)
+    header = next(reader)
+    tsTotal = wsTotal = precipTotal = rowCount = 0
+    for row in reader:
+        ts, ws, precip = minMaxNormalization(row)
+        tsTotal += ts
+        wsTotal += ws
+        precipTotal += precip
+        rowCount += 1
+tsAvg = tsTotal/rowCount
+wsAvg = wsTotal/rowCount
+precipAvg = precipTotal/rowCount
+print(tsAvg, wsAvg, precipAvg)    
 
 categories = ["Temp", "PFSI", "Precip", "Windspeed"]
-values = [0.4, 0.3, 0.5, 0.2]
+values = [tsAvg, 0.8, precipAvg, wsAvg]
 
 num_categories = len(categories)
 angles = np.linspace(0, 2 * np.pi, num_categories, endpoint=False).tolist()
