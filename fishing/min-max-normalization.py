19433d4990b200789bcf1bba4c791bf7a0f61673
hssboston <hanna.s.suzuki@gmail.com>
2025-10-03 23:54:57 -0400

    Update min-max-normalization.py

--- fishing/min-max-normalization.py
+++ fishing/min-max-normalization.py
@@ -2,10 +2,37 @@ import csv
 
 inputFile = "Tasmania_5years.csv"
 
+def minMaxNormalization(row):
+    ts, ws, precip = float(row[2]), float(row[5]), float(row[9])
+#     print(yr, doy, ts, ws, precip)
+    tsNormalized = 1 - abs(21-ts)/11
+    if ws >= 6:
+        ws = 6
+    wsNormalized = 1 - abs(3-ws)/6
+    if precip >= 10:
+        precip = 10
+    precipNormalized = 1 - precip/10
+    return (tsNormalized, wsNormalized, precipNormalized)
+
+
 with open(inputFile, "r") as f:
     reader = csv.reader(f)
-    rows = [row for row in reader]
-    csvHeaderRow = rows[0]
-    csvDataRows = rows[1:]
-print("Read", inputFile)
+    header = next(reader)
+#     rows = [row for row in reader]
+    tsTotal = wsTotal = precipTotal = rowCount = 0
+    for row in reader:
+        ts, ws, precip = minMaxNormalization(row)
+        tsTotal += ts
+        wsTotal += ws
+        precipTotal += precip
+        rowCount += 1
+tsAvg = tsTotal/rowCount
+wsAvg = wsTotal/rowCount
+precipAvg = precipTotal/rowCount
+print(tsAvg, wsAvg, precipAvg)
+    
+        
+
+
+
 
