7fa51e9fff81177a065ada689da5e64d8436a171
hssboston <hanna.s.suzuki@gmail.com>
2025-10-04 00:54:08 -0400

    rader

--- fishing/min-max-normalization.py
+++ fishing/min-max-normalization.py
@@ -5,7 +5,6 @@ inputFile = "Tasmania_5years.csv"
 with open(inputFile, "r") as f:
     reader = csv.reader(f)
     header = next(reader)
-#     rows = [row for row in reader]
     tempMaxTotal = tempMinTotal = precipTotal = rowCount = 0
     for row in reader:
         tempMax, tempMin, precip = float(row[7]), float(row[8]), float(row[9])
@@ -18,14 +17,21 @@ tempMinAvg = tempMinTotal/rowCount
 tempDiffAvg = tempMaxAvg - tempMinAvg
 precipAvg = precipTotal/rowCount
 print("Daily avg:", "Max temp", tempMaxAvg, "Temp diff", tempDiffAvg, "Precip", precipAvg)
-    
-tempMaxNormalized = 1 - abs(21 - tempMaxAvg)/13
 
-if tempDiff > 11:
-    tempDiff = 11
-tempDiffNormalized = 1- tempDiff/11
+if tempMaxAvg > 33:
+    tempMaxAvg = 33
+elif tempMaxAvg < 13:
+    tempMaxAvg = 13
+tempMaxNormalized = 1 - abs(23 - tempMaxAvg)/11
 
+if tempDiffAvg > 11:
+    tempDiffAvg = 11
+tempDiffNormalized = 1- tempDiffAvg/11
 
+if precipAvg > 10:
+    precipAvg = 10
+precipNormarized = 1 - precipAvg/11
 
+print("Normalized:", "Max temp", tempMaxNormalized, "Temp diff", tempDiffNormalized, "Precip", precipNormarized)
 
 
--- fishing/rader.py
+++ fishing/rader.py
@@ -2,39 +2,47 @@ import csv, numpy as np, matplotlib.pyplot as plt
 
 inputFile = "Tasmania_5years.csv"
 
-def minMaxNormalization(row):
-    ts, ws, precip = float(row[2]), float(row[5]), float(row[9])
-    tsNormalized = 1 - abs(21-ts)/11
-    wsNormalized = 1 - abs(3-ws)/6
-    if precip >= 10:
-        precip = 10
-    precipNormalized = 1 - precip/10
-    return (tsNormalized, wsNormalized, precipNormalized)
-
 with open(inputFile, "r") as f:
     reader = csv.reader(f)
     header = next(reader)
-    tsTotal = wsTotal = precipTotal = rowCount = 0
+    tempMaxTotal = tempMinTotal = precipTotal = rowCount = 0
     for row in reader:
-        ts, ws, precip = minMaxNormalization(row)
-        tsTotal += ts
-        wsTotal += ws
+        tempMax, tempMin, precip = float(row[7]), float(row[8]), float(row[9])
+        tempMaxTotal += tempMax
+        tempMinTotal += tempMin
         precipTotal += precip
         rowCount += 1
-tsAvg = tsTotal/rowCount
-wsAvg = wsTotal/rowCount
+tempMaxAvg = tempMaxTotal/rowCount
+tempMinAvg = tempMinTotal/rowCount
+tempDiffAvg = tempMaxAvg - tempMinAvg
 precipAvg = precipTotal/rowCount
-print(tsAvg, wsAvg, precipAvg)    
+print("Daily avg:", "Max temp", tempMaxAvg, "Temp diff", tempDiffAvg, "Precip", precipAvg)
 
-categories = ["Temp", "PFSI", "Precip", "Windspeed"]
-values = [tsAvg, 0.8, precipAvg, wsAvg]
+if tempMaxAvg > 33:
+    tempMaxAvg = 33
+elif tempMaxAvg < 13:
+    tempMaxAvg = 13
+tempMaxNormalized = 1 - abs(23 - tempMaxAvg)/11
+
+if tempDiffAvg > 11:
+    tempDiffAvg = 11
+tempDiffNormalized = 1- tempDiffAvg/11
+
+if precipAvg > 10:
+    precipAvg = 10
+precipNormarized = 1 - precipAvg/11
+
+print("Normalized:", "Max temp", tempMaxNormalized, "Temp diff", tempDiffNormalized, "Precip", precipNormarized)
+
+categories = ["Temp Diff", "Max Temp", "PFSI", "Precip"]
+values = [tempDiffNormalized, tempMaxNormalized, 0.8, precipNormarized]
 
 num_categories = len(categories)
 angles = np.linspace(0, 2 * np.pi, num_categories, endpoint=False).tolist()
 angles += angles[:1]
 values += values[:1]
 
-fig, ax = plt.subplots(figsize=(6, 6), subplot_kw=dict(polar=True))
+fig, ax = plt.subplots(figsize=(8, 6), subplot_kw=dict(polar=True))
 ax.plot(angles, values, linewidth=1, linestyle="solid", label="Tasmania")
 ax.fill(angles, values, "blue", alpha=0.1)
 
