cd4205b980a8efa9ebaf2d81c12518574207e1c9
hssboston <hanna.s.suzuki@gmail.com>
2025-11-14 23:25:13 -0500

    Update random-band.py

--- markov-chains/prep/random-band.py
+++ markov-chains/prep/random-band.py
@@ -46,59 +46,64 @@ for chord in progression:
         else:
             chordTrack.append(Message('note_off', note=note, velocity=50, time=0))
 
-# # Melody
-# #   Chord notes at 70% and scale notes at 30%
-# #   8th and 16th notes at random
-# for chord in progression:
-#     chordNotes = chords[chord]
-#     remainingTicks = note4
-#     
-#     while remainingTicks > 0:
-#         duration = random.choice([note8, note16])
-#         if remainingTicks - duration < 0:
-#             duration = remainingTicks        
-#         
-#         if random.random() < 0.7:
-#             note = random.choice(chordNotes)
-#         else:
-#             note = random.choice(scale)
-#             
-#         melodyTrack.append(Message('note_on', note=note, velocity=80, time=0))
-#         melodyTrack.append(Message('note_off', note=note, velocity=80, time=duration))
-#         
-#         remainingTicks -= duration
-# 
-# # Bass
-# for chord in progression:
-#     root = chords[chord][0] - 24
-#     remainingTicks = note4
-#     
-#     while remainingTicks > 0:
-#         duration = random.choice([note4, note8])
-#         if remainingTicks - duration < 0:
-#             duration = remainingTicks
-#         
-#         bassTrack.append(Message('note_on', note=root, velocity=90, time=0))
-#         bassTrack.append(Message('note_off', note=root, velocity=90, time=duration))
-#         remainingTicks -= duration
+# Melody
+#   Chord notes at 70% and scale notes at 30%
+#   8th and 16th notes at random
+for chord in progression:
+    chordNotes = chords[chord]
+    remainingTicks = note4
     
-# Dram
+    while remainingTicks > 0:
+        duration = random.choice([note8, note16])
+        if remainingTicks - duration < 0:
+            duration = remainingTicks        
+        
+        if random.random() < 0.7:
+            note = random.choice(chordNotes)
+        else:
+            note = random.choice(scale)
+            
+        melodyTrack.append(Message('note_on', note=note, velocity=80, time=0))
+        melodyTrack.append(Message('note_off', note=note, velocity=80, time=duration))
+        
+        remainingTicks -= duration
+
+# Bass
+for chord in progression:
+    root = chords[chord][0] - 24
+    remainingTicks = note4
+    
+    while remainingTicks > 0:
+        duration = random.choice([note4, note8])
+        if remainingTicks - duration < 0:
+            duration = remainingTicks
+        
+        bassTrack.append(Message('note_on', note=root, velocity=90, time=0))
+        bassTrack.append(Message('note_off', note=root, velocity=90, time=duration))
+        remainingTicks -= duration
+    
+# Dram kick and snare
 for _ in progression:
     for step in range(0, note4, note16):
         # Kick
         if step == 0:
-            drumTrack.append(Message('note_on', note=36, velocity=100, time=0, channel=9))
-            drumTrack.append(Message('note_off', note=36, velocity=100, time=note16, channel=9))
+            drumTrack.append(Message('note_on', note=36, velocity=80, time=0, channel=9))
+            drumTrack.append(Message('note_off', note=36, velocity=80, time=note16, channel=9))
         # Snare
-        if(step == note16) or (step == note16*3):
-            drumTrack.append(Message('note_on', note=38, velocity=100, time=0, channel=9))
-            drumTrack.append(Message('note_off', note=38, velocity=100, time=note16, channel=9))
+        elif(step == note16) or (step == note16*3):
+            drumTrack.append(Message('note_on', note=38, velocity=80, time=0, channel=9))
+            drumTrack.append(Message('note_off', note=38, velocity=80, time=note16, channel=9))
+        else:
+            drumTrack.append(Message('note_on', note=0, velocity=80, time=0, channel=9))
+            drumTrack.append(Message('note_off', note=0, velocity=80, time=note16, channel=9))
+            
+# Dram high hut
+for _ in progression:
+    for step in range(0, note4, note16):
         # Hihut
-        drumTrack.append(Message('note_on', note=42, velocity=70, time=0, channel=9))
-        drumTrack.append(Message('note_off', note=42, velocity=70, time=note16, channel=9))
+        drumTrack.append(Message('note_on', note=42, velocity=50, time=0, channel=9))
+        drumTrack.append(Message('note_off', note=42, velocity=50, time=note16, channel=9))
         
-
-
 midi.save(outputFile)
 print("Generated a MIDI file:", outputFile)
 
