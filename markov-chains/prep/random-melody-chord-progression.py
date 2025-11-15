b5810e94e38f54964030d33fc1823141b945a676
hssboston <hanna.s.suzuki@gmail.com>
2025-11-14 20:41:07 -0500

    Update random-melody-chord-progression.py

--- markov-chains/prep/random-melody-chord-progression.py
+++ markov-chains/prep/random-melody-chord-progression.py
@@ -13,39 +13,40 @@ chords = {
 progression = ["I", "V", "VI", "IV"]
 
 # MIDI setup
-mid = MidiFile()
-ch_track = MidiTrack()
-mel_track = MidiTrack()
-mid.tracks.append(ch_track)
-mid.tracks.append(mel_track)
+# ticks_per_beat=480
+midi = MidiFile()
+chordTrack = MidiTrack()
+melodyTrack = MidiTrack()
+midi.tracks.append(chordTrack)
+midi.tracks.append(melodyTrack)
 
-ch_track.append(MetaMessage('set_tempo', tempo=mido.bpm2tempo(100)))
-mel_track.append(MetaMessage('set_tempo', tempo=mido.bpm2tempo(100)))
+chordTrack.append(MetaMessage('set_tempo', tempo=mido.bpm2tempo(100)))
+melodyTrack.append(MetaMessage('set_tempo', tempo=mido.bpm2tempo(100)))
 
-# Generating chord progression
+# Chord progression
 time_per_chord = 480
 for chord in progression:
     notes = chords[chord]
     for note in notes:    
-        ch_track.append(Message('note_on', note=note, velocity=60, time=0))
+        chordTrack.append(Message('note_on', note=note, velocity=60, time=0))
     for i, note in enumerate(notes):
         if i==0:
-            ch_track.append(Message('note_off', note=note, velocity=60, time=time_per_chord))
+            chordTrack.append(Message('note_off', note=note, velocity=60, time=time_per_chord))
         else:
-            ch_track.append(Message('note_off', note=note, velocity=60, time=0))
+            chordTrack.append(Message('note_off', note=note, velocity=60, time=0))
 
-# Generating melody
+# Melody
 for chord in progression:
     # 4 eighth notes per chord
     for _ in range(4): 
         note = random.choice(scale)
-        mel_track.append(Message('note_on', note=note, velocity=80, time=0))
-        mel_track.append(Message('note_off', note=note, velocity=80, time=120))
+        melodyTrack.append(Message('note_on', note=note, velocity=80, time=0))
+        melodyTrack.append(Message('note_off', note=note, velocity=80, time=120))
 
-mid.save("auto_song.mid")
+midi.save("auto_song.mid")
 print("Generated a MIDI file: auto_song.mid")
 
-# mid = MidiFile('auto_song.mid')
+# midi = MidiFile('auto_song.mid')
 # for i, track in enumerate(mid.tracks):
 #     print('Track {}: {}'.format(i, track.name))
 #     for msg in track:
