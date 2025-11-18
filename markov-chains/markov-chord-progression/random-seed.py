4b9b4fb122831f9ad25a80966501892d3cd75df5
hssboston <hanna.s.suzuki@gmail.com>
2025-11-18 08:37:28 -0500

    Update random-seed.py

--- markov-chains/markov-chord-progression/random-seed.py
+++ markov-chains/markov-chord-progression/random-seed.py
@@ -14,8 +14,23 @@ print(random.random(), random.random(), random.random())
 print("Numpy")
 rng = np.random.default_rng()
 print(rng.random(), rng.random(), rng.random())
+rng = np.random.default_rng(seed=200)
+print(rng.random(), rng.random(), rng.random())
 
-# np.random
-# 
-# 
-# np.random.default_rng()
+rng = np.random.default_rng(seed=200); print(rng.random())
+rng = np.random.default_rng(seed=200); print(rng.random())
+rng = np.random.default_rng(seed=200); print(rng.random())
+
+l = ["apple", "orange", "grape"]
+rng = np.random.default_rng()
+print( rng.choice(l), rng.choice(l), rng.choice(l), rng.choice(l) )
+rng = np.random.default_rng(seed=300)
+print( rng.choice(l), rng.choice(l), rng.choice(l), rng.choice(l) )
+
+rng = np.random.default_rng(seed=300); print(rng.choice(l))
+rng = np.random.default_rng(seed=300); print(rng.choice(l))
+rng = np.random.default_rng(seed=300); print(rng.choice(l))
+
+rng = np.random.default_rng(seed=100); print(rng.choice(l))
+rng = np.random.default_rng(seed=100); print(rng.choice(l))
+rng = np.random.default_rng(seed=100); print(rng.choice(l))
