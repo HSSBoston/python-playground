f6a8ef7c0787cd22f459099794d4646b01f2f972
hssboston <hanna.s.suzuki@gmail.com>
2025-11-18 08:10:55 -0500

    Update random-seed.py

--- markov-chains/markov-chord-progression/random-seed.py
+++ markov-chains/markov-chord-progression/random-seed.py
@@ -6,7 +6,8 @@ print(random.random(), random.random(), random.random(),
 
 random.seed(100);  print(random.random())
 random.seed(100);  print(random.random())
-random.seed(None); print(random.random())
+# if seed=None, random() uses different seeds every time it is called.
+random.seed(None); print(random.random()) 
 
 print(random.random(), random.random(), random.random()) 
 
