f421156da5d301eb26e1485594bbd89d5aee8f05
hssboston <hanna.s.suzuki@gmail.com>
2025-11-19 10:58:59 -0500

    Update chap06-nested-streams.py

--- markov-chains/prep/music21/chap06-nested-streams.py
+++ markov-chains/prep/music21/chap06-nested-streams.py
@@ -1,4 +1,17 @@
 from music21 import *
+from pprint import pprint
 
+sBach = corpus.parse('bach/bwv57.8')
+
+print( len(sBach) )
+pprint( [e for e in sBach] )
+# print( len(sBach[3]) )
+# print( len(sBach[3][1]) )
+
+print( len(sBach.getElementsByClass(stream.Part)) )
+print( len(sBach.getElementsByClass(stream.Part)) )
+
+
+sBach.show("text")
 
 
