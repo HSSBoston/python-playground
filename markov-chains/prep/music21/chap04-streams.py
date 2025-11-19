64e2d47315efd9f4030927b2edb1fd36c9b16b50
hssboston <hanna.s.suzuki@gmail.com>
2025-11-19 10:13:16 -0500

    Create chap04-streams.py

---
+++ markov-chains/prep/music21/chap04-streams.py
@@ -0,0 +1,27 @@
+from music21 import *
+
+note1 = note.Note("C4")
+note2 = note.Note("F#4")
+note3 = note.Note("B-2")
+
+stream1 = stream.Stream()
+stream1.append(note1)
+stream1.append(note2)
+stream1.append(note3)
+
+stream1.show("text")
+# stream1.show()
+
+print(stream1)
+print( stream1[0], stream1[-1], stream1[-2] )
+print( stream1[0:3] )
+
+stream1.pop( stream1.index(note2) )
+stream1.append(note2)
+# stream1.show()
+
+for n in stream1:
+    print(n.offset, n.name)
+
+# stream1.show("midi")
+
