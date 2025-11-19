74bfb9c7ed7f8c84449e93a9981406c5f0081cd8
hssboston <hanna.s.suzuki@gmail.com>
2025-11-19 09:10:24 -0500

    Update chap02-notes.py

--- markov-chains/prep/music21/chap02-notes.py
+++ markov-chains/prep/music21/chap02-notes.py
@@ -2,7 +2,7 @@ from music21 import note
 
 f = note.Note("F5")
 print(f, f.name, f.octave, f.nameWithOctave)
-print(f.pitch, f.pitch.frequency, f.pitch.pitchClassString, f.duration)
+print(f.pitch, f.pitch.frequency, f.pitch.pitchClassString, f.pitch.midi, f.duration)
 
 # f.show()
 
