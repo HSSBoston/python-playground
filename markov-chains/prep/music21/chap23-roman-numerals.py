ad39a16ece778801ef23c3ac2e69baf25b997968
hssboston <hanna.s.suzuki@gmail.com>
2025-11-19 18:44:43 -0500

    Update chap23-roman-numerals.py

--- markov-chains/prep/music21/chap23-roman-numerals.py
+++ markov-chains/prep/music21/chap23-roman-numerals.py
@@ -1,2 +1,17 @@
 from music21 import *
 
+f = chord.Chord('F4 C5 A5')
+kf = key.Key('F')
+sf = stream.Measure([kf, f])
+# sf.show()
+
+kc = key.Key('C')
+sc = stream.Part([kc, f])
+# sc.show()
+
+rf = roman.romanNumeralFromChord(f, kf)
+print(rf)
+
+rc = roman.romanNumeralFromChord(f, kc)
+print(rc)
+
