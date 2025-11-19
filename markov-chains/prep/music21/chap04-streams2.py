4a4f1f22fc22a9f6954f4ee8b70051c79d3db27f
hssboston <hanna.s.suzuki@gmail.com>
2025-11-19 10:30:06 -0500

    Update chap04-streams2.py

--- markov-chains/prep/music21/chap04-streams2.py
+++ markov-chains/prep/music21/chap04-streams2.py
@@ -1,3 +1,19 @@
 from music21 import *
 
+note1 = note.Note("C4")
+note2 = note.Note("F#4")
+note3 = note.Note("B-2")
+
+stream1 = stream.Stream()
+stream1.append(note1)
+stream1.append(note2)
+stream1.append(note3)
+
+biggerStream = stream.Stream()
+biggerStream.insert(0, note2)
+biggerStream.append(stream1)
+
+biggerStream.show("text")
+
+
 
