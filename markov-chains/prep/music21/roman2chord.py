edb20d223a643765fe4c4fd9b9be1bdc967d51c4
hssboston <hanna.s.suzuki@gmail.com>
2025-11-20 13:47:18 -0500

    Update roman2chord.py

--- markov-chains/prep/music21/roman2chord.py
+++ markov-chains/prep/music21/roman2chord.py
@@ -2,32 +2,38 @@ from music21 import *
 
 keyChoice = "C"
 
-roman.RomanNumeral("I", keyChoice)
+rc1 = roman.RomanNumeral("I", keyChoice)
+rc2 = roman.RomanNumeral("ii", keyChoice)
+rc4 = roman.RomanNumeral("IV", keyChoice)
+rc5 = roman.RomanNumeral("V", keyChoice)
+rc6 = roman.RomanNumeral("vi", keyChoice)
+rc7 = roman.RomanNumeral("viio", keyChoice)
+chords = [rc1, rc2, rc4, rc5, rc6, rc7]
+
+print ([p.midi for p in rc1.pitches])
+
+romanToChordMidi = {
+    "I",  [p.midi for p in rc1.pitches]
+    "II", [p.midi for p in rc2.pitches]
+    "IV", [p.midi for p in rc4.pitches]
+    "V",  [p.midi for p in rc5.pitches]
+    "VI", [p.midi for p in rc6.pitches]
+    "VII",[p.midi for p in rc7.pitches]
+    }
+
+
+print(rc1.figure, rc1.figureAndKey, rc1.romanNumeralAlone, rc1.key, rc1.scaleDegree)
+print(rc1.functionalityScore)
+if rc1.figure=="I":
+    print("Yes", rc1.figure)
+if rc1.scaleDegree==1:
+    print("Yes", rc1.scaleDegree)
 
 
 
-f = chord.Chord('F4 C5 A5')
-kf = key.Key('F')
-sf = stream.Measure([kf, f])
-# sf.show()
 
-kc = key.Key('C')
-sc = stream.Part([kc, f])
-# sc.show()
-
-rf = roman.romanNumeralFromChord(f, kf)
-print(rf)
-
-rc = roman.romanNumeralFromChord(f, kc)
-print(rc)
-print(rc.figure, rc.figureAndKey, rc.romanNumeralAlone, rc.key, rc.scaleDegree)
-print(rc.functionalityScore)
-
-rc2 = roman.RomanNumeral("I", kc)
-print(rc2)
-
-rc3 = rc2 = roman.RomanNumeral("I", "C")
-print(rc3)
-
-
+for chord in chords:
+    for p in chord.pitches:
+        print(chord.figure, p, p.midi)
+    
 
