9a5c629abf9be79e064bcf665c5d7830cc37b02f
hssboston <hanna.s.suzuki@gmail.com>
2025-11-20 14:13:49 -0500

    Update roman2chord.py

--- markov-chains/prep/music21/roman2chord.py
+++ markov-chains/prep/music21/roman2chord.py
@@ -1,4 +1,5 @@
 from music21 import *
+from pprint import pprint
 
 keyChoice = "C"
 
@@ -10,16 +11,24 @@ rc6 = roman.RomanNumeral("vi", keyChoice)
 rc7 = roman.RomanNumeral("viio", keyChoice)
 chords = [rc1, rc2, rc4, rc5, rc6, rc7]
 
-print ([p.midi for p in rc1.pitches])
-
 romanToChordMidi = {
-    "I",  [p.midi for p in rc1.pitches]
-    "II", [p.midi for p in rc2.pitches]
-    "IV", [p.midi for p in rc4.pitches]
-    "V",  [p.midi for p in rc5.pitches]
-    "VI", [p.midi for p in rc6.pitches]
-    "VII",[p.midi for p in rc7.pitches]
+    "I":  [p.midi for p in rc1.pitches],
+    "II": [p.midi for p in rc2.pitches],
+    "IV": [p.midi for p in rc4.pitches],
+    "V":  [p.midi for p in rc5.pitches],
+    "VI": [p.midi for p in rc6.pitches],
+    "VII":[p.midi for p in rc7.pitches],
     }
+pprint(romanToChordMidi)
+romanToChordName = {
+    "I":  [p.nameWithOctave for p in rc1.pitches],
+    "II": [p.nameWithOctave for p in rc2.pitches],
+    "IV": [p.nameWithOctave for p in rc4.pitches],
+    "V":  [p.nameWithOctave for p in rc5.pitches],
+    "VI": [p.nameWithOctave for p in rc6.pitches],
+    "VII":[p.nameWithOctave for p in rc7.pitches],    
+    }
+pprint(romanToChordName)
 
 
 print(rc1.figure, rc1.figureAndKey, rc1.romanNumeralAlone, rc1.key, rc1.scaleDegree)
@@ -28,9 +37,6 @@ if rc1.figure=="I":
     print("Yes", rc1.figure)
 if rc1.scaleDegree==1:
     print("Yes", rc1.scaleDegree)
-
-
-
 
 for chord in chords:
     for p in chord.pitches:
