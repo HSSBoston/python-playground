71c07d0ac87c366b59ffd5b8c87b2c99fd6c89e9
hssboston <hanna.s.suzuki@gmail.com>
2025-11-21 07:24:53 -0500

    Update roman2chord.py

--- markov-chains/prep/music21/roman2chord.py
+++ markov-chains/prep/music21/roman2chord.py
@@ -1,9 +1,9 @@
 from music21 import *
 from pprint import pprint
 
-keyChoice = "C"
+keyChoice = "c"
 
-if keyChoice in ["C","D","E","F","G","A","B"]
+if keyChoice in ["C","D","E","F","G","A","B"]:
     rc1 = roman.RomanNumeral("I", keyChoice)
     rc2 = roman.RomanNumeral("ii", keyChoice)
     rc4 = roman.RomanNumeral("IV", keyChoice)
@@ -11,27 +11,14 @@ if keyChoice in ["C","D","E","F","G","A","B"]
     rc6 = roman.RomanNumeral("vi", keyChoice)
     rc7 = roman.RomanNumeral("viio", keyChoice)
 
-if keyChoice in ["c","d","e","f,"g","a","b"]
-    rc1 = roman.RomanNumeral("I", keyChoice)
-    rc2 = roman.RomanNumeral("ii", keyChoice)
-    rc4 = roman.RomanNumeral("IV", keyChoice)
-    rc5 = roman.RomanNumeral("V", keyChoice)
-    rc6 = roman.RomanNumeral("vi", keyChoice)
-    rc7 = roman.RomanNumeral("viio", keyChoice)
+if keyChoice in ["c","d","e","f","g","a","b"]:
+    rc1 = roman.RomanNumeral("i", keyChoice)
+    rc2 = roman.RomanNumeral("iio", keyChoice)
+    rc4 = roman.RomanNumeral("iv", keyChoice)
+    rc5 = roman.RomanNumeral("v", keyChoice)
+    rc6 = roman.RomanNumeral("VI", keyChoice)
+    rc7 = roman.RomanNumeral("VII", keyChoice)
 
-chords = [rc1, rc2, rc4, rc5, rc6, rc7]
-
-    
-
-romanToChordMidi = {
-    "I":  [p.midi for p in rc1.pitches],
-    "II": [p.midi for p in rc2.pitches],
-    "IV": [p.midi for p in rc4.pitches],
-    "V":  [p.midi for p in rc5.pitches],
-    "VI": [p.midi for p in rc6.pitches],
-    "VII":[p.midi for p in rc7.pitches],
-    }
-pprint(romanToChordMidi)
 romanToChordName = {
     "I":  [p.nameWithOctave for p in rc1.pitches],
     "II": [p.nameWithOctave for p in rc2.pitches],
@@ -42,6 +29,15 @@ romanToChordName = {
     }
 pprint(romanToChordName)
 
+romanToChordMidi = {
+    "I":  [p.midi for p in rc1.pitches],
+    "II": [p.midi for p in rc2.pitches],
+    "IV": [p.midi for p in rc4.pitches],
+    "V":  [p.midi for p in rc5.pitches],
+    "VI": [p.midi for p in rc6.pitches],
+    "VII":[p.midi for p in rc7.pitches],
+    }
+pprint(romanToChordMidi)
 
 print(rc1.figure, rc1.figureAndKey, rc1.romanNumeralAlone, rc1.key, rc1.scaleDegree)
 print(rc1.functionalityScore)
@@ -50,6 +46,7 @@ if rc1.figure=="I":
 if rc1.scaleDegree==1:
     print("Yes", rc1.scaleDegree)
 
+chords = [rc1, rc2, rc4, rc5, rc6, rc7]
 for chord in chords:
     for p in chord.pitches:
         print(chord.figure, p, p.midi)
