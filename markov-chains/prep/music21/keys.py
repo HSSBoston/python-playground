830a59cb7d864a5b75cc89bc93d54fb478165b2c
hssboston <hanna.s.suzuki@gmail.com>
2025-11-21 10:10:28 -0500

    Update keys.py

--- markov-chains/prep/music21/keys.py
+++ markov-chains/prep/music21/keys.py
@@ -5,11 +5,15 @@ keyChoices = ["C", "c", "a"]
 for kc in keyChoices:    
     k = key.Key(kc)
     print(k.tonic, k.mode)
-    print( [k.pitchFromDegree(i).nameWithOctave for i in range(1,7)] )
-    print( [k.pitchFromDegree(i).midi for i in range(1,7)] )    
+    print( [k.pitchFromDegree(i).nameWithOctave for i in range(1,8)] )
+    print( [k.pitchFromDegree(i).midi for i in range(1,8)] )    
 
-cm = key.Key("a")
-print(cm.mode, cm.tonic)
-for i in range(1, 7):
-    print( cm.pitchFromDegree(i) )
+am = key.Key("a")
+print(am.tonic, am.mode)
+print("Relative:", am.relative)
+print("Parallel", am.parallel)
+print( [am.pitchFromDegree(i).nameWithOctave for i in range(1,8)] )
+
+am3 = am.transpose(-12)
+print( [am3.pitchFromDegree(i).transpose(-12).nameWithOctave for i in range(1,8)] )
              
