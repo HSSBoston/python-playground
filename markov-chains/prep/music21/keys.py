23ae53084a168ffad6d7841a4aa4179c5bc4c4fa
hssboston <hanna.s.suzuki@gmail.com>
2025-11-21 12:16:19 -0500

    Update keys.py

--- markov-chains/prep/music21/keys.py
+++ markov-chains/prep/music21/keys.py
@@ -31,4 +31,6 @@ print( [am.pitchFromDegree(i).nameWithOctave for i in range(1,8)] )
 
 am3 = am.transpose(-12)
 print( [am3.pitchFromDegree(i).transpose(-12).nameWithOctave for i in range(1,8)] )
-             
+print(am3.tonic, am3.mode)
+
+
