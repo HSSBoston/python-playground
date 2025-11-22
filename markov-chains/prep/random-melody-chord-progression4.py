da952b548945c7624c6628c9c5ed75042dae234b
hssboston <hanna.s.suzuki@gmail.com>
2025-11-22 06:59:16 -0500

    Update random-melody-chord-progression4.py

--- markov-chains/prep/random-melody-chord-progression4.py
+++ markov-chains/prep/random-melody-chord-progression4.py
@@ -4,7 +4,8 @@ from music21 import *
 from pprint import pprint
 
 KEY_CHOICE = "a"
-PROGRESSION = ["I", "IV", "V", "I"]
+PROGRESSION = ["I", "IV", "VII", "I", "VI", "II", "V", "I"]
+# PROGRESSION = ["I", "IV", "V", "I"]
 # PROGRESSION = ["I", "V", "VI", "IV"]
 
 BPM = 100
@@ -25,37 +26,71 @@ scaleMidi = [p.midi for p in scale.getPitches()[:-1]]
 print(f"Scale (MIDI)\t {scaleMidi}" )
  
 if k.mode == "major":
-    rc1 = roman.RomanNumeral("I", scale)
-    rc2 = roman.RomanNumeral("ii", scale)
-    rc4 = roman.RomanNumeral("IV", scale)
-    rc5 = roman.RomanNumeral("V", scale)
-    rc6 = roman.RomanNumeral("vi", scale)
-    rc7 = roman.RomanNumeral("viio", scale)
+    rn1 = roman.RomanNumeral("I", scale)
+    rn2 = roman.RomanNumeral("ii", scale)
+    rn4 = roman.RomanNumeral("IV", scale)
+    rn5 = roman.RomanNumeral("V", scale)
+    rn6 = roman.RomanNumeral("vi", scale)
+    rn7 = roman.RomanNumeral("viio", scale)
+
+    rn17 = roman.RomanNumeral("I7", scale)
+    rn27 = roman.RomanNumeral("ii7", scale)
+    rn47 = roman.RomanNumeral("IV7", scale)
+    rn57 = roman.RomanNumeral("V7", scale)
+    rn67 = roman.RomanNumeral("vi7", scale)
+    rn77 = roman.RomanNumeral("viio7", scale)
 else:
-    rc1 = roman.RomanNumeral("i", scale)
-    rc2 = roman.RomanNumeral("iio", scale)
-    rc4 = roman.RomanNumeral("iv", scale)
-    rc5 = roman.RomanNumeral("v", scale)
-    rc6 = roman.RomanNumeral("VI", scale)
-    rc7 = roman.RomanNumeral("VII", scale)
+    rn1 = roman.RomanNumeral("i", scale)
+    rn2 = roman.RomanNumeral("iio", scale)
+    rn4 = roman.RomanNumeral("iv", scale)
+    rn5 = roman.RomanNumeral("v", scale)
+    rn6 = roman.RomanNumeral("VI", scale)
+    rn7 = roman.RomanNumeral("VII", scale)
+
+    rn17 = roman.RomanNumeral("i7", scale)
+    rn27 = roman.RomanNumeral("iio7", scale)
+    rn47 = roman.RomanNumeral("iv7", scale)
+    rn57 = roman.RomanNumeral("v7", scale)
+    rn67 = roman.RomanNumeral("VI7", scale)
+    rn77 = roman.RomanNumeral("VII7", scale)
 
 romanToChordName = {
-    "I":  [p.nameWithOctave for p in rc1.pitches],
-    "II": [p.nameWithOctave for p in rc2.pitches],
-    "IV": [p.nameWithOctave for p in rc4.pitches],
-    "V":  [p.nameWithOctave for p in rc5.pitches],
-    "VI": [p.nameWithOctave for p in rc6.pitches],
-    "VII":[p.nameWithOctave for p in rc7.pitches],    
+    "I":  [p.nameWithOctave for p in rn1.pitches],
+    "II": [p.nameWithOctave for p in rn2.pitches],
+    "IV": [p.nameWithOctave for p in rn4.pitches],
+    "V":  [p.nameWithOctave for p in rn5.pitches],
+    "VI": [p.nameWithOctave for p in rn6.pitches],
+    "VII":[p.nameWithOctave for p in rn7.pitches],    
     }
 pprint(romanToChordName)
 
+romanTo7thChordName = {
+    "I":  [p.nameWithOctave for p in rn17.pitches],
+    "II": [p.nameWithOctave for p in rn27.pitches],
+    "IV": [p.nameWithOctave for p in rn47.pitches],
+    "V":  [p.nameWithOctave for p in rn57.pitches],
+    "VI": [p.nameWithOctave for p in rn67.pitches],
+    "VII":[p.nameWithOctave for p in rn77.pitches],    
+    }
+pprint(romanTo7thChordName)
+
 romanToChordMidi = {
-    "I":  [p.midi for p in rc1.pitches],
-    "II": [p.midi for p in rc2.pitches],
-    "IV": [p.midi for p in rc4.pitches],
-    "V":  [p.midi for p in rc5.pitches],
-    "VI": [p.midi for p in rc6.pitches],
-    "VII":[p.midi for p in rc7.pitches],
+    "I":  [p.midi for p in rn1.pitches],
+    "II": [p.midi for p in rn2.pitches],
+    "IV": [p.midi for p in rn4.pitches],
+    "V":  [p.midi for p in rn5.pitches],
+    "VI": [p.midi for p in rn6.pitches],
+    "VII":[p.midi for p in rn7.pitches],
+    }
+pprint(romanToChordMidi)
+
+romanToChordMidi = {
+    "I":  [p.midi for p in rn17.pitches],
+    "II": [p.midi for p in rn27.pitches],
+    "IV": [p.midi for p in rn47.pitches],
+    "V":  [p.midi for p in rn57.pitches],
+    "VI": [p.midi for p in rn67.pitches],
+    "VII":[p.midi for p in rn77.pitches],
     }
 pprint(romanToChordMidi)
 
@@ -87,27 +122,27 @@ for roman in PROGRESSION :
         else:
             chordTrack.append(Message('note_off', note=note, velocity=60, time=0))
 
-# # Melody
-# #   Chord notes at 70% and scale notes at 30%
-# #   8th and 16th notes at random
-# for chord in progression:
-#     chordNotes = chords[chord]
-#     remainingTicks = note4
-#     
-#     while remainingTicks > 0:
-#         duration = random.choice([note8, note16])
-#         if remainingTicks - duration < 0:
-#             duration = remainingTicks        
-#         
-#         if random.random() < 0.7:
-#             note = random.choice(chordNotes)
-#         else:
-#             note = random.choice(scale)
-#             
-#         melodyTrack.append(Message('note_on', note=note, velocity=80, time=0))
-#         melodyTrack.append(Message('note_off', note=note, velocity=80, time=duration))
-#         
-#         remainingTicks -= duration
+# Melody
+#   Chord notes at 70% and scale notes at 30%
+#   8th and 16th notes at random
+for chord in progression:
+    chordNotes = chords[chord]
+    remainingTicks = note4
+    
+    while remainingTicks > 0:
+        duration = random.choice([note8, note16])
+        if remainingTicks - duration < 0:
+            duration = remainingTicks        
+        
+        if random.random() < 0.7:
+            note = random.choice(chordNotes)
+        else:
+            note = random.choice(scale)
+            
+        melodyTrack.append(Message('note_on', note=note, velocity=80, time=0))
+        melodyTrack.append(Message('note_off', note=note, velocity=80, time=duration))
+        
+        remainingTicks -= duration
 
 midi.save(OUTPUT_FILE)
 print("Generated a MIDI file:", OUTPUT_FILE)
