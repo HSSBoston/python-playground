95537093fa3034c3a17961d432343ade24d56a9f
hssboston <hanna.s.suzuki@gmail.com>
2025-12-04 08:53:24 -0500

    Update weighted-choice.py

--- markov-chains/markov-chord-progression/weighted-choice.py
+++ markov-chains/markov-chord-progression/weighted-choice.py
@@ -7,14 +7,11 @@ noteChoices = [["C", 1.5],
 def weightedChoice(choices):
     totalWeight = sum([w for _, w in choices])
     print(totalWeight)
-    if total == 0:
-        return random.choice([item for item, _ in choices])
-    r = random.uniform(0, totalWeight) # random float in [0, totalWeight]
-    upto = 0
-    for item, weight in choices:
-        if upto + weight >= r:
-            return item
-        upto += weight
-    return choices[-1][0]
+    if totalWeight == 0:
+        return random.choice([item for item, weight in choices])
+    else:
+        return random.choices([item   for (item, weight) in choices],
+                              weights=[weight for (item, weight) in choices],
+                              k=1)
 
 print( weightedChoice(noteChoices) )
